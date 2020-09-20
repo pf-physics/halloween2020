7f1976f60a56d386dc8c41eeb624a13b240dc160
pf-physics <perryfagant.sacha@gmail.com>
2020-09-20 13:24:00 +0200

    Updates

---
+++ precache-manifest.2f958fb7ac1b30e84cfc3b6e49f7792d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2e856de7be9bf14e3969ed7b20be5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be18983c8c5ccda169f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7916ed425628f0efabdf",
+    "url": "/static/js/2.c0f8aa36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c0f8aa36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be18983c8c5ccda169f",
+    "url": "/static/js/main.ffcff06b.chunk.js"
+  },
+  {
+    "revision": "44bc8d80288b73ac01c4",
+    "url": "/static/js/runtime-main.3f806d2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
